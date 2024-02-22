6ee642908cd2646f4cb68b0239f53682d1ca1627
Kevin Li <qingtian805@126.com>
2024-02-22 18:44:25 +0800

    finish speed test

--- test/functest/speedtest.c
+++ test/functest/speedtest.c
@@ -7,13 +7,14 @@ int main(void)
     clock_t start, end;
     double gbps;
     int i;
-    union sm4data key = {0x01234567, 0x89ABCDEF, 0xFEDCBA98, 0x76543210};
-    union sm4data data = {0x01234567, 0x89ABCDEF, 0xFEDCBA98, 0x76543210};
+    uint32_t key[4] = {0x01234567, 0x89ABCDEF, 0xFEDCBA98, 0x76543210};
+    uint32_t data[4] = {0x01234567, 0x89ABCDEF, 0xFEDCBA98, 0x76543210};
+    uint32_t data_out[4];
 
     start = clock();
     
     for(i = 0; i < 8388608; i++)
-        sm4(data, key);
+        sm4_enc(data, data_out, key);
 
     end = clock();
 
